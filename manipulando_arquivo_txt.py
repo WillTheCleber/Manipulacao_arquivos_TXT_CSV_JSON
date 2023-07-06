b89ebb92ff16fd62b77891a416f50d546498f0f0
WillTheCleber <williamclebercorreia@gmail.com>
2023-07-06 19:28:06 -0300

    primeiro commit

---
+++ manipulando_arquivo_txt.py
@@ -0,0 +1,23 @@
+#vamos abri um arquivo para leitura
+
+arqu1= open('arquivos/arquivo.txt','r')
+
+print(arqu1.read())
+
+arqu1.seek(0) #Aqui estou retornando o cursor para a linha 0 
+
+print(arqu1.read)
+
+arqu1.close()
+
+arqu2 = open('arquivos/arquivo.txt','w+')
+
+arqu2.write("Temos novos conteudos \n")
+arqu2.write("Tem novo conteudo escrito de novo \n")
+
+arqu2.close
+
+arq3 = open('arquivos/arquivo.txt', 'a+')
+
+arq3.write("Nova escrita sem excluir.")
+
